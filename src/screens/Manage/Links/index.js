ccb85304b0c400080c533a18bda1079643e77ed3
FranciscoMaik <chiconunes3081@gmail.com>
2020-07-25 17:48:32 -0300

    Deletar implementado

--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -3,12 +3,28 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import Layout from "../../Layout/Manage";
-import { linkList, setLinkToRemove } from "../../../actions/LinkActions";
+import {
+  linkList,
+  setLinkToRemove,
+  linkRemove,
+} from "../../../actions/LinkActions";
 
-const Links = ({ links, linkToRemove, linkList, setLinkToRemove }) => {
+const Links = ({
+  links,
+  linkRemove,
+  linkToRemove,
+  linkList,
+  setLinkToRemove,
+}) => {
   useEffect(() => {
     linkList();
   }, [linkList]);
+
+  const cancelDelete = (e) => {
+    setLinkToRemove(null);
+  };
+
+  const confirmDelete = (e) => (linkToRemove ? linkRemove(linkToRemove) : null);
 
   return (
     <Layout>
@@ -56,6 +72,22 @@ const Links = ({ links, linkToRemove, linkList, setLinkToRemove }) => {
             );
           })
         : null}
+
+      {linkToRemove ? (
+        <div className="alert alert-danger rounded float-center shadow-bold">
+          <h4 className="alert-heading">Delete Confirmation!</h4>
+          <p>Are you sure you want to delete, this action cannot be undone</p>
+          <hr />
+          <div className="d-flex justify-content-between">
+            <button className="btn btn-outline-light" onClick={cancelDelete}>
+              cancel
+            </button>
+            <button className="btn btn-danger" onClick={confirmDelete}>
+              delete
+            </button>
+          </div>
+        </div>
+      ) : null}
     </Layout>
   );
 };
@@ -64,4 +96,8 @@ const mapStateToProps = (state) => {
   return { links: state.link.links, linkToRemove: state.link.linkToRemove };
 };
 
-export default connect(mapStateToProps, { linkList, setLinkToRemove })(Links);
+export default connect(mapStateToProps, {
+  linkList,
+  setLinkToRemove,
+  linkRemove,
+})(Links);
